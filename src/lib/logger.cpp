33380438ed9c58dfa3c50b7ff55b31c7af12033d
Pavel Vácha <pavel.vachaaa@gmail.com>
2025-09-18 10:20:43 +0200

    fix: nested name specifier warning

--- src/lib/logger.cpp
+++ src/lib/logger.cpp
@@ -1,58 +1,59 @@
 #include "logger.hpp"
 
-void Logger::debug(const std::string& message)
+void Logger::debug(const std::string &message)
 {
-	Logger::log(Severity::DEBUG, message);
+    Logger::log(DEBUG, message);
 }
 
-void Logger::info(const std::string& message)
+void Logger::info(const std::string &message)
 {
-	Logger::log(Severity::INFO, message);
+    Logger::log(INFO, message);
 }
 
-void Logger::warning(const std::string& message)
+void Logger::warning(const std::string &message)
 {
-	Logger::log(Severity::WARNING, message);
+    Logger::log(WARNING, message);
 }
 
-void Logger::error(const std::string& message)
+void Logger::error(const std::string &message)
 {
-	Logger::log(Severity::ERR, message);
+    Logger::log(ERR, message);
 }
 
-void Logger::critical(const std::string& message)
+void Logger::critical(const std::string &message)
 {
-	Logger::log(Severity::CRITICAL, message);
+    Logger::log(CRITICAL, message);
 }
 
-void Logger::log(Severity severity, const std::string& message)
+void Logger::log(Severity severity, const std::string &message)
 {
     {
         std::time_t now = std::time(nullptr);
         std::tm tm_now;
-    
-        #if defined(_WIN32) || defined(_WIN64)
-            localtime_s(&tm_now, &now);   // Windows
-        #else
-            localtime_r(&now, &tm_now);   // Linux/Unix
-        #endif
+
+#if defined(_WIN32) || defined(_WIN64)
+        localtime_s(&tm_now, &now); // Windows
+#else
+        localtime_r(&now, &tm_now); // Linux/Unix
+#endif
 
         std::cout << "[" << std::put_time(&tm_now, "%Y-%m-%d %H:%M:%S") << "] ";
 
-        switch (severity) {
-        case Severity::DEBUG:
+        switch (severity)
+        {
+        case DEBUG:
             std::cout << "[DEBUG]";
             break;
-        case Severity::INFO:
+        case INFO:
             std::cout << "[INFO]";
             break;
-        case Severity::WARNING:
+        case WARNING:
             std::cout << "[WARNING]";
             break;
-        case Severity::ERR:
+        case ERR:
             std::cout << "[ERROR]";
             break;
-        case Severity::CRITICAL:
+        case CRITICAL:
             std::cout << "[CRITICAL]";
             break;
         }
