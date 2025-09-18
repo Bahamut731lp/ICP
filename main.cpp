4baf236f0752a2bdf76846d924b9f0e3f04dde75
Pavel Vácha <pavel.vachaaa@gmail.com>
2025-09-18 11:21:21 +0200

    feat:  opencv setup test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include "src/lib/logger.hpp"
+#include <opencv2/opencv.hpp>
 
 int main()
 {
+    cv::Mat img = cv::imread("test.jpg");
+    if (img.empty())
+    {
+        std::cout << "Could not read the image\n";
+        return 1;
+    }
+    cv::imshow("Image", img);
+    cv::waitKey(0);
     Logger::info("New Application has been constructed.");
     return 0;
 }
